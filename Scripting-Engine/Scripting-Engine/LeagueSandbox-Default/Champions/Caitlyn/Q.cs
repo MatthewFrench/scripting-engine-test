c3dd778a29266fffe44d61737bef9fea8fe0dc6d
Crauzer <thenatokenproject@gmail.com>
2017-01-22 09:44:44 +0100

    Caitlyn Q, R

--- Scripting-Engine/Scripting-Engine/LeagueSandbox-Default/Champions/Caitlyn/Q.cs
+++ Scripting-Engine/Scripting-Engine/LeagueSandbox-Default/Champions/Caitlyn/Q.cs
@@ -14,13 +14,20 @@ namespace Caitlyn
         {
         
         }
-        static void onFinishCasting()
+        static void onFinishCasting(Champion owner, Spell spell)
         {
-        
+            Vector2 current = new Vector2(owner.X, owner.Y);
+            Vector2 to = Vector2.Normalize(new Vector2(spell.X, spell.Y) - current);
+            Vector2 range = to * 1150;
+            Vector2 trueCoords = current + range;
+
+            spell.AddProjectile("CaitlynPiltoverPeacemaker", trueCoords.X, trueCoords.Y, true);
         }
-        static void applyEffects()
+        static void applyEffects(Champion owner, Spell spell, Projectile projectile)
         {
-        
+            int reduc = Math.Min(projectile.ObjectsHit.Count, 5);
+            owner.dealDamageTo(spell.Target, (spell.getEffectValue(0) + owner.GetStats().AttackDamage.Total + (1.3f * owner.GetStats().AttackDamage.Total)) + spell.getEffectValue(0) * (1 - reduc / 10), DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+            ApiFunctionManager.AddParticleTarget(owner, "caitlyn_Base_peaceMaker_tar_02.troy", spell.Target);
         }
         static void onUpdate(double diff)
         {
--- Scripting-Engine/Scripting-Engine/LeagueSandbox-Default/Champions/Caitlyn/R.cs
+++ Scripting-Engine/Scripting-Engine/LeagueSandbox-Default/Champions/Caitlyn/R.cs
@@ -14,13 +14,22 @@ namespace Caitlyn
         {
         
         }
-        static void onFinishCasting()
+        static void onFinishCasting(Champion owner, Spell spell, Unit castTarget)
         {
-        
+            Vector2 current = new Vector2(owner.X, owner.Y);
+            if(Vector2.Distance(current, new Vector2(castTarget.X, castTarget.Y)) <= spell.getEffectValue(1))
+            {
+                spell.AddProjectileTarget("CaitlynAceintheHoleMissile", castTarget);
+            }
         }
-        static void applyEffects()
+        static void applyEffects(Champion owner, Spell spell, Unit castTarget, Projectile projectile)
         {
-        
+            if(castTarget != null && ApiFunctionManager.IsDead(castTarget))
+            {
+                float damage = spell.getEffectValue(0) + owner.GetStats().AttackDamage.Total * 2;
+                owner.dealDamageTo(castTarget, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+            }
+            projectile.setToRemove();
         }
         static void onUpdate(double diff)
         {
