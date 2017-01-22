8bc5af6966079c418ca175b72930a8c77f4aac69
Crauzer <thenatokenproject@gmail.com>
2017-01-22 07:45:00 +0100

    Lua 2 CS

---
+++ Scripting-Engine/Scripting-Engine/LeagueSandboxLua2CS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueSandboxLua2CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LeagueSandbox-Default Location || classType(static etc....)");
+            string[] input = Console.ReadLine().Split(' ');
+            ClassWriter classWriter = new ClassWriter(input[0], input[1]);
+        }
+    }
+}
