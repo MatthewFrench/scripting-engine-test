99f598f7ae81347a6a3a4c0b432189a072699edd
Crauzer <thenatokenproject@gmail.com>
2017-01-22 09:12:25 +0100

    Karthus R

--- Scripting-Engine/Scripting-Engine/LeagueSandbox-Default/Champions/Karthus/R.cs
+++ Scripting-Engine/Scripting-Engine/LeagueSandbox-Default/Champions/Karthus/R.cs
@@ -10,13 +10,21 @@ namespace Karthus
 {
     class R
     {
-        static void onStartCasting()
+        static void onStartCasting(Champion owner)
         {
-        
+            foreach(Champion target in ApiFunctionManager.GetChampionsInRange(owner, 20000, true).Where(target => target.Team != owner.Team))
+            {
+                ApiFunctionManager.AddParticleTarget(owner, "KarthusFallenOne", target);
+            }
         }
-        static void onFinishCasting()
+        static void onFinishCasting(Champion owner, Spell spell)
         {
-        
+            float AP = owner.GetStats().AbilityPower.Total;
+            float damage = 100 + (spell.Level * 150) + AP * 0.6f;
+            foreach (Champion target in ApiFunctionManager.GetChampionsInRange(owner, 20000, true).Where(target => target.Team != owner.Team))
+            {
+                owner.dealDamageTo(target, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+            }
         }
         static void applyEffects()
         {
