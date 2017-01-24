e1c9e5d48b2ba6c3ee6dd9fb4a3686dcf86146dd
MatthewFrench <matthewf@ksu.edu>
2017-01-23 19:32:39 -0600

    Working on a class fixer

---
+++ Scripting-Engine/Scripting-Engine/LeagueSandboxLua2CS/ClassFixer.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LeagueSandboxLua2CS
+{
+    public class ClassFixer
+    {
+        public ClassFixer(string folder)
+        {
+            String[] allfiles = System.IO.Directory.GetFiles(folder, "*.cs", System.IO.SearchOption.AllDirectories);
+
+            for (var i = 0; i < allfiles.Length; i++)
+            {
+                String fileLocation = allfiles[i];
+                String fileName = fileLocation;
+                String spellName = fileName.Substring(fileName.LastIndexOf("\\") + 1);
+                spellName = spellName.Substring(0, spellName.Length - 3);
+                String championName = fileName.Substring(0, fileName.LastIndexOf("\\"));
+                championName = championName.Substring(championName.LastIndexOf("\\") + 1);
+                Console.WriteLine("Champion Name: " + championName);
+                Console.WriteLine("Spell Name: " + spellName);
+
+                String oldScript = "";
+                String fixedScript = "";
+
+                if (spellName != "Q" && spellName != "W" && spellName != "E" && spellName != "R")
+                {
+                    if (spellName == "Passive") {
+                     fixedScript = @"using LeagueSandbox.GameServer.Logic.GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace "+ championName + @"
+{
+    public class " + spellName + @"
+    {
+        public static void onUpdate(double diff) { }
+        public static void onDamageTaken(Unit attacker, float damage, DamageType type, DamageSource source) { }
+        public static void onAutoAttack(Unit target) { }
+        public static void onDealDamage(Unit target, float damage, DamageType damageType, DamageSource source) { }
+        public static void onSpellCast(float x, float y, Spell slot, Unit target) { }
+        public static void onDie(Unit killer) { }
+    }
+}
+";
+                    } else
+                    {
+                        continue;
+                    }
+                } else
+                {
+                    using (StreamReader sr = new StreamReader(fileName))
+                    {
+                        oldScript = sr.ReadToEnd();
+                    }
+
+                    String innerOnStartCasting = "";
+                    String innerOnFinishCasting = "";
+                    String innerApplyEffects = "";
+                    String innerOnUpdate = "";
+                    int locationOfFunctionStart;
+                    if (oldScript.IndexOf("onStartCasting") != -1)
+                    {
+
+                         locationOfFunctionStart = oldScript.IndexOf("{", oldScript.IndexOf("onStartCasting")) + 1;
+                        innerOnStartCasting = oldScript.Substring(locationOfFunctionStart, oldScript.IndexOf("}", locationOfFunctionStart) - locationOfFunctionStart);
+
+                    }
+                    if (oldScript.IndexOf("onFinishCasting") != -1)
+                    {
+                        locationOfFunctionStart = oldScript.IndexOf("{", oldScript.IndexOf("onFinishCasting")) + 1;
+                        innerOnFinishCasting = oldScript.Substring(locationOfFunctionStart, oldScript.IndexOf("}", locationOfFunctionStart) - locationOfFunctionStart);
+                    }
+                    if (oldScript.IndexOf("applyEffects") != -1)
+                    {
+                        locationOfFunctionStart = oldScript.IndexOf("{", oldScript.IndexOf("applyEffects")) + 1;
+                        innerApplyEffects = oldScript.Substring(locationOfFunctionStart, oldScript.IndexOf("}", locationOfFunctionStart) - locationOfFunctionStart);
+                    }
+                    if (oldScript.IndexOf("onUpdate") != -1)
+                    {
+                        locationOfFunctionStart = oldScript.IndexOf("{", oldScript.IndexOf("onUpdate")) + 1;
+                        innerOnUpdate = oldScript.Substring(locationOfFunctionStart, oldScript.IndexOf("}", locationOfFunctionStart) - locationOfFunctionStart);
+                    }
+                     fixedScript = @"using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using System.Text;
+ using System.Threading.Tasks;
+ using System.Numerics;
+ using LeagueSandbox.GameServer.Logic.GameObjects;
+ using LeagueSandbox.GameServer.Logic.API;
+
+ namespace " + championName + @"
+ {
+     public class " + spellName + @"
+     {
+         public static void onStartCasting(Champion owner)
+         {
+" + innerOnStartCasting + @"
+         }
+         public static void onFinishCasting(Champion owner, Spell spell)
+         {
+" + innerOnFinishCasting + @"
+         }
+         public static void applyEffects(Champion owner, Unit target, Spell spell, Projectile projectile)
+         {
+" + innerApplyEffects + @"
+         }
+         public static void onUpdate(double diff) {
+       " + innerOnUpdate + @"  
+         }
+     }
+ }";
+                }
+                
+
+                Console.WriteLine("Old script: " + oldScript + "[End Old Script]");
+                Console.WriteLine("New Script: " + fixedScript + "[End New Script]");
+
+                System.IO.File.WriteAllText(fileLocation, fixedScript);
+            }
+            
+        }
+    }
+}
--- Scripting-Engine/Scripting-Engine/LeagueSandboxLua2CS/Program.cs
+++ Scripting-Engine/Scripting-Engine/LeagueSandboxLua2CS/Program.cs
@@ -10,10 +10,23 @@ namespace LeagueSandboxLua2CS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("LeagueSandbox-Default Location || classType(static etc....) || removeFilesBeforeWriting(true, false)");
-            string[] input = Console.ReadLine().Split(' ');
-            ClassWriter classWriter = new ClassWriter(input[0], input[1], input[2]);
-            classWriter.Write();
+            Console.WriteLine("Press A for class writer and B for class fixer: ");
+
+            ConsoleKeyInfo num = Console.ReadKey();
+
+            if (num.Key == ConsoleKey.A)
+            {
+                Console.WriteLine("LeagueSandbox-Default Location || classType(static etc....) || removeFilesBeforeWriting(true, false)");
+                string[] input = Console.ReadLine().Split(' ');
+                ClassWriter classWriter = new ClassWriter(input[0], input[1], input[2]);
+                classWriter.Write();
+            } else
+            {
+                Console.WriteLine("\nInput LeagueSandbox-Default directory: ");
+                String directory = Console.ReadLine();
+                ClassFixer classFixer = new ClassFixer(directory);
+            }
+            
         }
     }
 }
